93a27ea9b1654b1db09fe6bccd5125e467a0cd04
Matthew Walter <mwalter@ttic.edu>
2018-08-30 16:50:04 -0500

    moved robot-status from husky repo to its own repo

--- c/lcmtypes/robot_status_lcmtypes.h
+++
@@ -1,9 +0,0 @@
-#ifndef __lcmtypes_robot_status_h__
-#define __lcmtypes_robot_status_h__
-
-#include "ripl_heartbeat_t.h"
-#include "ripl_robot_state_command_t.h"
-#include "ripl_comment_t.h"
-#include "ripl_robot_status_t.h"
-
-#endif
