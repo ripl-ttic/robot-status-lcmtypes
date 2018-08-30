ce8e3c38765ec83444a6564f00e7d40a66c9d1a2
Matthew Walter <mwalter@ttic.edu>
2018-08-30 16:48:16 -0500

    moved robot-status-related lcmtypes to the robot-status pod

---
+++ cpp/lcmtypes/robot_status.hpp
@@ -0,0 +1,9 @@
+#ifndef __lcmtypes_robot_status_hpp__
+#define __lcmtypes_robot_status_hpp__
+
+#include "ripl/robot_state_command_t.hpp"
+#include "ripl/robot_status_t.hpp"
+#include "ripl/comment_t.hpp"
+#include "ripl/heartbeat_t.hpp"
+
+#endif
